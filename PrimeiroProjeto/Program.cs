1b4e183c67c9fe05c9fd5fb3771b5c24b90cf898
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-21 09:32:44 -0300

    Creating the final feature of the application, the option to display the average of the bands
    Implementing the latest feature of the C# application, which is the option to display the average ratings of the registered bands. For this purpose, the 'ExibirMediaDeUmBanda' function was created, which takes input from the user regarding which band they want to see the average ratings for. It then checks if this band is registered and, if it is, provides information about the ratings the band has received and, ultimately, what the rating average for that band is.

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -20,6 +20,15 @@ void ExibirLogo()
     Console.WriteLine(mensagemDeBoasVindas);
 }
 
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
 void ExibirOpcoesDoMenu()
 {
     ExibirLogo();
@@ -45,7 +54,7 @@ void ExibirOpcoesDoMenu()
             AvaliarUmaBanda();
             break;
         case 4:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            ExibirMediaDeUmaBanda();
             break;
         case -1:
             Console.WriteLine("Tchau tchau :)");
@@ -91,15 +100,6 @@ void MostrarBandasRegistradas()
     ExibirOpcoesDoMenu();
 }
 
-void ExibirTituloDaOpcao(string titulo)
-{
-    int quantidadeDeLetras = titulo.Length;
-    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-    Console.WriteLine(asteriscos);
-    Console.WriteLine(titulo);
-    Console.WriteLine(asteriscos + "\n");
-}
-
 void AvaliarUmaBanda()
 {
     //digite qual banda deseja avaliar
@@ -117,7 +117,7 @@ void AvaliarUmaBanda()
         int nota = int.Parse(Console.ReadLine()!);
         bandasRegistradas[nomeDaBanda].Add(nota);
         Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-        Thread.Sleep(2000);
+        Thread.Sleep(4000);
     }
     else
     {
@@ -128,6 +128,42 @@ void AvaliarUmaBanda()
     Console.Clear();
     ExibirOpcoesDoMenu();
 
+}
+
+void ExibirMediaDeUmaBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir a média de uma banda");
+    Console.Write("Digite o nome da banda que deseja ver a média de avaliações: ");
+    string nomeDaBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        List<int> listaDeAvaliacoesDaBanda = bandasRegistradas[nomeDaBanda];
+
+        string listaDeAvaliacoesDaBandaString = "[";
+
+        foreach (int nota in listaDeAvaliacoesDaBanda)
+        {
+            listaDeAvaliacoesDaBandaString += nota + ", ";
+        }
+
+        listaDeAvaliacoesDaBandaString = listaDeAvaliacoesDaBandaString.Trim(' ', ',');
+        listaDeAvaliacoesDaBandaString += "]";
+
+        double mediaDaBanda = listaDeAvaliacoesDaBanda.Average();
+        Console.WriteLine($"\nA banda {nomeDaBanda} recebeu as notas {listaDeAvaliacoesDaBandaString}");
+        Console.WriteLine($"A média da banda {nomeDaBanda} é: {mediaDaBanda}");
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
 }
 
 ExibirOpcoesDoMenu();
